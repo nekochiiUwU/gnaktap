f1217fe0991330441eae8e02896ebfa125cee54a
tremisabdoul <75373856+tremisabdoul@users.noreply.github.com>
2023-07-09 19:19:01 +0200

    C'est une boite manuelle

--- Player.gd
+++ Player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody3D
 
 
-var gravity:   float = -ProjectSettings.get_setting("physics/3d/default_gravity")
-var air_speed: float =  50.
-var jump:      float =  7.
-var speed:     float =  5.
-var sensi:   Vector2 = -Vector2(.005, .005)
+var gravity:     float = -ProjectSettings.get_setting("physics/3d/default_gravity")
+var air_speed:   float =  50.
+var jump:        float =  7.
+var speed:       float =  5.
+var sensi:     Vector2 = -Vector2(.005, .005)
+var joy_sensi: Vector2 = -Vector2(.1, .1)
 
 
 func _ready():
@@ -29,6 +30,12 @@ func _input(event):
 
 func _physics_process(delta):
 	if is_multiplayer_authority():
+		var input_rotation = Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down")
+		if input_rotation:
+			rotation.y += input_rotation.x * joy_sensi.x
+			$Head.rotation.x = clamp(-1.57, $Head.rotation.x + input_rotation.y * joy_sensi.y, 1.57)
+			$Head.rotation.z += input_rotation.x * joy_sensi.x / 10
+			get_node("Head/Camera").fov = min(get_node("Head/Camera").fov + abs(input_rotation.x * sensi.x * 5), 160)
 		#print(velocity.dot(transform.basis.x))
 		var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").normalized()
 		input = Vector3(input.x, 0, input.y)
@@ -50,11 +57,10 @@ func _physics_process(delta):
 			velocity.x /= 11
 			velocity.z /= 11
 
-
 		#print("Speed: ", float(int(sqrt(pow(velocity.x, 2) + pow(velocity.z, 2)) * 1000)) / 1000)
 
 		move_and_slide()
-		
+
 		$Head.rotation.z -= velocity.dot(transform.basis.x) / 1000
 		$Head.rotation.z /= 1.1
 		$Head/UI.scale = Vector3.ONE * .25 + Vector3.ONE * float(int(sqrt(pow(velocity.x, 2) + pow(velocity.z, 2)) * 1000)) / 1000 / 20
@@ -62,3 +68,10 @@ func _physics_process(delta):
 		$Head/UI.scale += Vector3.ONE * float(!is_on_floor()) * .1
 		$Head/UI.scale.z = 1 
 		get_node("Head/Camera").fov = (get_node("Head/Camera").fov - 120) / 1.2 + 120
+		
+		rpc("online_syncronisation", position)
+
+
+@rpc("authority", "call_remote", "unreliable", 0)
+func online_syncronisation(_position: Vector3):
+	position = _position
