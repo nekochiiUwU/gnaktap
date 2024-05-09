643fb9f3e22b503f42be6daa03dbb539addc9925
tremisabdoul <tremisabdoul@gmail.com>
2024-05-09 03:59:16 +0200

    visée + rename Player.gd

--- root/game/entities/player/weapons/M4A1/M4A1.gd
+++ root/game/entities/player/weapons/M4A1/M4A1.gd
@@ -6,8 +6,10 @@ func primary_process(_delta):
 
 
 func secondary_process(_delta):
-	pass
+	position += Vector3(.02, -.12, -0.65) * _delta*10
+	position /= 1 + _delta*10
 
 
 func idle_process(_delta):
-	pass
+	position += Vector3(0.25, -0.2, -0.667) * _delta*10
+	position /= 1 + _delta*10
